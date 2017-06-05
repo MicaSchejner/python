991344980f80728d6c78ea7b2a7d1c1ebd9d70e3
MICA <micaela.sch.aquino@gmail.com>
2017-06-05 16:19:22 -0300

    agregar archivos

---
+++ merge_list.py
@@ -0,0 +1,19 @@
+def merge_lists(a,b):
+    # len_a = len(a)
+    # len_b = len(b)
+    #
+    # for i in
+    a.extend(b)
+    rta = sorted(a)
+
+
+
+    return rta
+
+
+
+my_list     = [3, 4, 6, 10, 11, 15,19]
+alices_list = [1, 5, 8, 12, 14, 19]
+
+print merge_lists(my_list, alices_list)
+# prints [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
