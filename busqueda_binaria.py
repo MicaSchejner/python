466f8f853d6b533889efd20a2edfca31a1ae0a14
MICA <micaela.sch.aquino@gmail.com>
2017-06-13 12:38:43 -0300

    la puta madre

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -4,8 +4,24 @@ def buscar_sumas(list,num):
     der = len(list)-1
     medio = (izq+der)/2
 
-    if list[medio]<=7:
-        der = medio -1
+    print "izq"
+    print izq
+    print "der"
+    print der
+    print "medio"
+    print list[medio]
+    if list[medio]<=num:
+        der = medio + 1
+        print list[:der]
+    else:
+        izq = medio + 1
+        print list[izq:]
+
+
+    return list[:der]
+
+list = [1,3,4,7,8,10]
+print buscar_sumas(list, 10)
 
 
     
